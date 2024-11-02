0d2b621d3af096f02dbd490da8150fc48900d3df
mahsanajafi <mahsa.najafi2006@gmail.com>
2024-11-03 01:04:03 +0330

    feat/terms&conditions

---
+++ src/stores/adminStore.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+interface IIsAdmin {
+    isAdmin: boolean,
+    setIsAdmin: (isAdmin: boolean) => void
+}
+const adminStore = create<IIsAdmin>((set)=>({
+    isAdmin:localStorage.getItem("isAdmin")==='true'?true:false,
+    setIsAdmin:(val:boolean)=>{
+        localStorage.setItem("isAdmin",String(val));
+        set({isAdmin:val}
+    )}
+
+}))
+
+export default adminStore
